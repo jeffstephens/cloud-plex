7ff160d2202a1f9f01fa9a2d988915018126fcf5
Jeff Stephens <jeff@jeffastephens.com>
2020-10-11 12:11:39 -0700

    Update Plex version

--- terraform/data/provision.sh
+++ terraform/data/provision.sh
@@ -3,6 +3,6 @@
 sudo apt-get update -qq
 sudo apt-get -y upgrade
 
-wget -O plex.deb https://downloads.plex.tv/plex-media-server-new/1.19.4.2935-79e214ead/debian/plexmediaserver_1.19.4.2935-79e214ead_amd64.deb
+wget -O plex.deb https://downloads.plex.tv/plex-media-server-new/1.20.3.3421-54b6523bd/debian/plexmediaserver_1.20.3.3421-54b6523bd_amd64.deb
 sudo dpkg -i plex.deb
 curl -sSL https://repos.insights.digitalocean.com/install.sh | sudo bash
